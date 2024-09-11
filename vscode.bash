672f5216391a4e33157bed40611f6cb79fdd229b
tux-box <nathan.flow@gmail.com>
2024-09-11 15:15:47 -0500

    adding line to install

--- vscode.bash
+++ vscode.bash
@@ -4,3 +4,4 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y --no-install-recommends tzdata gnome-keyring wget curl python3-minimal ca-certificates git build-essential 
 apt-get autoremove -y
 apt-get clean -y 
+curl https://aka.ms/install-vscode-server/setup.sh | sh
