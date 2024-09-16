ffd04a76cbe6e5b3dac63587d9a4e977057bfb01
tux-box <nathan.flow@gmail.com>
2024-09-16 10:23:13 -0500

    changing

--- vscode.bash
+++ vscode.bash
@@ -4,4 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y --no-install-recommends tzdata gnome-keyring wget curl python3-minimal ca-certificates git build-essential 
 apt-get autoremove -y
 apt-get clean -y 
-curl https://aka.ms/install-vscode-server/setup.sh | sh
+#curl https://aka.ms/install-vscode-server/setup.sh | sh
+wget -q -O- https://aka.ms/install-vscode-server/setup.sh | sh
